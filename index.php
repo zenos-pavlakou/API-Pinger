7a0120abc680b94f620d5439dd768dba9380fc09
Zenos Pavlakou <90039805+zenos-pavlakou@users.noreply.github.com>
2023-02-06 10:36:28 +0000

    Made usage of APIPinger clearer

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
   $pinger->disallow_unknown_endpoints(); //if an endpoint is not enlisted, then it is unknown.
   $pinger->allow_unknown_endpoints(); //now all endpoints can be pinged, even if not enlisted. Enabled by default.
 
+  //This enlisting does not have a function closure, so by default all data will be returned from the api response. 
+  $pinger->enlist_endpoint(
+    "https://ethgasstation.info/api/ethgasAPI.json"
+  );
+
   //The function closure in the second argument is invoked with the data from the api response. 
   $pinger->enlist_endpoint(
     "https://randomuser.me/api",
@@ -20,16 +25,10 @@
     }
   );
 
-  //This enlisting does not have a function closure, so by default all data will be returned from the api response. 
-  $pinger->enlist_endpoint(
-    "https://ethgasstation.info/api/ethgasAPI.json"
-  );
+  //This will return the gender, name and email of the random user, as specified by the function closure.
+  $pinger->ping("https://randomuser.me/api");
 
-  echo var_dump(
-    $pinger->ping(
-      "https://ethgasstation.info/api/ethgasAPI.json", 
-      $return_raw_data=false
-    )
-  );
+  //This will return the data from the API response without applying a function closure. 
+  $pinger->ping("https://randomuser.me/api", $return_raw_data=true);
 
 ?>

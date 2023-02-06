8568b2f80461fa4da9d2ca9f160c5655bb9a090d
Zenos Pavlakou <90039805+zenos-pavlakou@users.noreply.github.com>
2023-02-06 10:26:40 +0000

    Added comments to describe how to use the APIPinger object

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   $pinger->disallow_unknown_endpoints(); //if an endpoint is not enlisted, then it is unknown.
   $pinger->allow_unknown_endpoints(); //now all endpoints can be pinged, even if not enlisted. Enabled by default.
 
+  //The function closure in the second argument is invoked with the data from the api response. 
   $pinger->enlist_endpoint(
     "https://randomuser.me/api",
     function($data) {
@@ -19,15 +20,9 @@
     }
   );
 
+  //This enlisting does not have a function closure, so by default all data will be returned from the api response. 
   $pinger->enlist_endpoint(
-    "https://ethgasstation.info/api/ethgasAPI.json",
-    function($data) {
-      return [
-        "fastest" => $data["fastest"],
-        "fast" => $data["fast"],
-        "slow" => $data["safeLow"]
-      ];
-    }
+    "https://ethgasstation.info/api/ethgasAPI.json"
   );
 
   echo var_dump(
